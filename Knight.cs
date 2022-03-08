0fcb66538b8897746eb019cf54f1f3de11c1fed3
Aurea <89421463+aurea94@users.noreply.github.com>
2022-03-08 13:50:50 -0300

    dding the files to the repository

---
+++ Knight.cs
@@ -0,0 +1,12 @@
+namespace POO.src.Entities
+{
+    public class Knight : Hero
+    {
+        public Knight(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using POO.src.Entities;
+
+namespace POO
+{
+    class Program
+  {
+    static void Main(string[] args)
+    {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(wizard.Attack(1));
+    }
+  }
+}
